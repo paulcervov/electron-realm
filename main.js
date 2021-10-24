7327aa00cc86edaa66731902d89816030404add7
Paul Cervov <paulcervov@gmail.com>
2021-10-25 00:48:24 +0300

    WIP auto update setup

--- main.js
+++ main.js
@@ -1,13 +1,19 @@
 const Electron = require('electron');
-const path = require('path');
-const Realm = require('realm');
+const log = require('electron-log');
 const { autoUpdater } = require('electron-updater');
+const Realm = require('realm');
+const path = require('path');
 const {APP_ENV, APP_ID} = require('./app.config');
+
+log.info('App starting...');
+
+autoUpdater.logger = log;
+autoUpdater.logger.transports.file.level = 'info';
 
 process.chdir(Electron.app.getPath('userData'));
 
 async function createBrowserWindow() {
-    const browserWindow = new Electron.BrowserWindow({
+    let browserWindow = new Electron.BrowserWindow({
         width: 800,
         height: 600,
         webPreferences: {
@@ -20,6 +26,10 @@ async function createBrowserWindow() {
     if(APP_ENV === 'development') {
         browserWindow.webContents.openDevTools();
     }
+
+    browserWindow.on('closed', () => {
+        browserWindow = null;
+    })
 
     return browserWindow;
 }
@@ -81,12 +91,41 @@ Promise.all([
         Electron.app.quit()
     });
 
+
+    autoUpdater.on('checking-for-update', () => {
+        log.info('Checking for update...');
+        browserWindow.webContents.send('main', {message: 'Checking for update...'});
+    })
+    autoUpdater.on('update-available', (info) => {
+        log.info('Update available.');
+        browserWindow.webContents.send('main', {message: 'Update available.'});
+    })
+    autoUpdater.on('update-not-available', (info) => {
+        log.info('Update not available.');
+        browserWindow.webContents.send('main', {message: 'Update not available.'});
+    })
+    autoUpdater.on('error', (err) => {
+        log.info('Error in auto-updater. ' + err);
+        browserWindow.webContents.send('main', {message: 'Error in auto-updater. ' + err});
+    })
+    autoUpdater.on('download-progress', (progressObj) => {
+        let log_message = "Download speed: " + progressObj.bytesPerSecond;
+        log_message = log_message + ' - Downloaded ' + progressObj.percent + '%';
+        log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ')';
+        log.info(log_message);
+        browserWindow.webContents.send('main', {message: log_message});
+    })
+    autoUpdater.on('update-downloaded', (info) => {
+        log.info('Update downloaded');
+        browserWindow.webContents.send('main', {message: 'Update downloaded'});
+    });
+
+    browserWindow.webContents.send('main', {message: `App version: ${Electron.app.getVersion()}`});
+
     /*browserWindow.webContents.send('main', {message: 'Message from main!'});
     Electron.ipcMain.on('main', (event, data) => {
         console.log('Received from renderer, data: %s', JSON.stringify(data));
     });*/
-
-
 
     await autoUpdater.checkForUpdatesAndNotify();
 
